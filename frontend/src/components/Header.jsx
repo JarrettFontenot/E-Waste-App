ad42a0942e7ff5235c6178427d894022c43dc020
Jarrett <115436271+JarrettFontenot@users.noreply.github.com>
2024-04-27 15:36:12 -0500

    About page design

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,26 @@
-import React from "react";
-import '../styles.css';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
-    return (
-        <header>
-        <h1>E-Waste</h1>
-        <nav>
-          <ul>
-          <li><Link to="/about">About</Link></li> 
-            <li><Link to="/register">Sign up</Link></li> 
-            <li><a href="#">Log in</a></li>
-            <li><a href="#">Profile</a></li>
-          </ul>
-        </nav>
-        </header>
-    )
+  const location = useLocation();
+
+  return (
+    <header>
+      <h1>E-Waste</h1>
+      <nav>
+        <ul>
+          {location.pathname === '/about' ? (
+            <li><Link to="/">Home</Link></li>
+          ) : (
+            <li><Link to="/about">About</Link></li>
+          )}
+          <li><Link to="/register">Sign up</Link></li> 
+          <li><a href="#">Log in</a></li>
+          <li><a href="#">Profile</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
--- frontend/src/pages/AboutUsPage.jsx
+++ frontend/src/pages/AboutUsPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import '../styles.css';
+import Header from '../components/Header';
 
 function AboutUs() {
   return (
-    <div className='About-us-container'>
-      <h1>Welcome to the about us Page</h1>
-      <p>This is the about us page of our React app.</p>
+    <div className='About-Page'>
+    <Header />
+    <div className='About-First'>
+      <div className='First-Text'>
+        <h1>What is e-waste?</h1>
+        <p>Test</p>
+      </div>
+      <div className='First-Image'></div>
+    </div>
+    <div class="thin-white-line"></div>
+    <div className='About-Second'>
+      <div className='Second-Image'></div>
+      <div className='Second-Text'>
+        <h1>What is the problem?</h1>
+        <p>Test</p>
+      </div>
+    </div>
+    <div class="thin-white-line"></div>
+    <div className='About-Third'>
+      <div className='Third-Text'>
+        <h1>How to help!</h1>
+        <p>Test</p>
+      </div>
+      <div className='Third-Image'></div>
+    </div>
     </div>
   );
 }
