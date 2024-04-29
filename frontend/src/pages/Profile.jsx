f97eb35958a38c0e2072efacc3d215375ce7dc3b
Jarrett <115436271+JarrettFontenot@users.noreply.github.com>
2024-04-29 13:43:01 -0500

    Profile changes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles.css';
 import Header from '../components/Header';
 
-
 function Profile() {
+    const [contributions, setContributions] = useState(0);
+
+    const incrementContributions = () => {
+        setContributions(prevContributions => prevContributions + 1);
+    };
+
+    const decrementContributions = () => {
+        setContributions(prevContributions => prevContributions - 1);
+    };
+
     return (
         <div>
             <Header />
             <div className='Profile-Page'>
-                <h1>Profile</h1>
+                <h1>Your Profile</h1>
+                <div className='Profile-Container'>
+                    <h1>Your e-waste contributions</h1>
+                    <div className='Contribution-Counter'>
+                        <div className='Big-Number'>{contributions}</div>
+                        <div className='Plus-Sign' onClick={incrementContributions}>+</div>
+                        <div className='Minus-Sign' onClick={decrementContributions}>-</div>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Profile;
